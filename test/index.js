826f17dde2eed3bf755a871288c450b2a8cfd2eb
mark adel <mark.adell@outlook.com>
2018-01-09 18:43:48 +0200

    add more functionalities to random function
    - returns a random number between 0 and 1 if no parameter is passed
    - if one parameter is passed it returns a random number between 0 and it inclusive

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ describe('min function', () => {
     it('should return undefined if the array is empty', () => {
         expect(_.min([])).to.be.equal(undefined);
     });
-    it('should return undefined if no arguments were passed', () => {
+    it('should return undefined if no parameters were passed', () => {
         expect(_.min()).to.be.equal(undefined);
     });
 });
@@ -26,7 +26,7 @@ describe('max function', () => {
     it('should return undefined if the array is empty', () => {
         expect(_.max([])).to.be.equal(undefined);
     });
-    it('should return undefined if no arguments were passed', () => {
+    it('should return undefined if no parameters were passed', () => {
         expect(_.max()).to.be.equal(undefined);
     });
 });
@@ -38,7 +38,7 @@ describe('sum function', () => {
     it('should return 0 if the array is empty', () => {
         expect(_.sum([])).to.be.equal(0);
     });
-    it('should return 0 if no arguments were passed', () => {
+    it('should return 0 if no parameters were passed', () => {
         expect(_.sum()).to.be.equal(0);
     });
 });
@@ -50,13 +50,13 @@ describe('ceil function', () => {
     it('should return the ceil of a negative decimal', () => {
         expect(_.ceil(-2.5)).to.be.equal(-2);
     });
-    it('should return zero if the passed number is zero', () => {
+    it('should return zero if the passed parameter is zero', () => {
         expect(_.ceil(0)).to.be.equal(0);
     });
     it('should work if the string after coercion is a number', () => {
         expect(_.ceil(2.5)).to.be.equal(3);
     });
-    it('should return undefined if the passed number is not a number', () => {
+    it('should return undefined if the passed parameter is not a number', () => {
         expect(_.ceil("string")).to.be.equal(undefined);
     });
 });
@@ -68,32 +68,73 @@ describe('floor function', () => {
     it('should return the floor of a negative decimal', () => {
         expect(_.floor(-2.5)).to.be.equal(-3);
     });
-    it('should return zero if the passed number is zero', () => {
+    it('should return zero if the passed parameter is zero', () => {
         expect(_.floor(0)).to.be.equal(0);
     });
     it('should work if the string after coercion is a number', () => {
         expect(_.floor(2.5)).to.be.equal(2);
     });
-    it('should return undefined if the passed number is not a number', () => {
+    it('should return undefined if the passed parameter is not a number', () => {
         expect(_.floor("string")).to.be.equal(undefined);
     });
 });
 
 describe('random function', () => {
-    let randomsNumbers = [];
-    for (var i = 0; i < 10000; i++) {
-        randomsNumbers.push(_.random(1, 10));
+    let getUniqRandGenNums = numOfPars => {
+        let randomsNumbers = [];
+        let params = [[], [5], [1, 10]][numOfPars];
+        for (var i = 0; i < 10000; i++) {
+            randomsNumbers.push(_.random(...params));
+        }
+        randomsNumbers = [...new Set(randomsNumbers)];
+        let minRandom = _.min(randomsNumbers);
+        let maxRandom = _.max(randomsNumbers);
+        return {
+            randomsNumbers,
+            minRandom,
+            maxRandom
+        }
     }
-    randomsNumbers = [...new Set(randomsNumbers)];
-    let minRandom = _.min(randomsNumbers);
-    let maxRandom = _.max(randomsNumbers);
-    it('should return a random number in a range', () => {
-        expect(randomsNumbers.length).to.be.above(5);
+    describe('no patameters passed', () => {
+        let minRandom = getUniqRandGenNums(0).minRandom;
+        let maxRandom = getUniqRandGenNums(0).maxRandom;
+        let randomsNumbers = getUniqRandGenNums(0).randomsNumbers;
+        it('should return a random number in a range', () => {
+            expect(randomsNumbers.length).to.be.above(1);
+        });
+        it('should not generate a number below the lower bound', () => {
+            expect(minRandom).to.be.within(0, 1);
+        });
+        it('should not generate a number above the upper bound', () => {
+            expect(maxRandom).to.be.within(0, 1);
+        });
     });
-    it('should not generate a number below the lower bound', () => {
-        expect(minRandom).to.not.be.below(1);
+    describe('one patameter passed', () => {
+        let minRandom = getUniqRandGenNums(1).minRandom;
+        let maxRandom = getUniqRandGenNums(1).maxRandom;
+        let randomsNumbers = getUniqRandGenNums(1).randomsNumbers;
+        it('should return a random number in a range', () => {
+            expect(randomsNumbers.length).to.be.above(1);
+        });
+        it('should not generate a number below the lower bound', () => {
+            expect(minRandom).to.be.within(0, 5);
+        });
+        it('should not generate a number above the upper bound', () => {
+            expect(maxRandom).to.be.within(0, 5);
+        });
     });
-    it('should not generate a number above the upper bound', () => {
-        expect(maxRandom).to.not.be.above(10);
+    describe('two patameter passed', () => {
+        let minRandom = getUniqRandGenNums(2).minRandom;
+        let maxRandom = getUniqRandGenNums(2).maxRandom;
+        let randomsNumbers = getUniqRandGenNums(2).randomsNumbers;
+        it('should return a random number in a range', () => {
+            expect(randomsNumbers.length).to.be.above(1);
+        });
+        it('should not generate a number below the lower bound', () => {
+            expect(minRandom).to.be.within(1, 10);
+        });
+        it('should not generate a number above the upper bound', () => {
+            expect(maxRandom).to.be.within(1, 10);
+        });
     });
 });
